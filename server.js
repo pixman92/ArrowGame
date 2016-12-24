9b80374f1548401a6b836b1767552bf46a5091b7
ChristianBear <pixman92@gmail.com>
2016-12-23 22:00:23 -0800

    updating server port: 80, necessary to put 'app.listen'

--- server.js
+++ server.js
@@ -16,24 +16,24 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
-app.get("/dreams", function (request, response) {
-  response.send(dreams);
-});
+// app.get("/dreams", function (request, response) {
+//   response.send(dreams);
+// });
 
-// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
-app.post("/dreams", function (request, response) {
-  dreams.push(request.query.dream);
-  response.sendStatus(200);
-});
+// // could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
+// app.post("/dreams", function (request, response) {
+//   dreams.push(request.query.dream);
+//   response.sendStatus(200);
+// });
 
-// Simple in-memory store for now
-var dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
+// // Simple in-memory store for now
+// var dreams = [
+//   "Find and count some sheep",
+//   "Climb a really tall mountain",
+//   "Wash the dishes"
+// ];
 
 // listen for requests :)
-var listener = 8000, function () {
+var listener = app.listen(8000, function () {
   console.log('Your app is listening on port ' + 8000);
 });
